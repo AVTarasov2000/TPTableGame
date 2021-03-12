c70144f313acf746e148651ec95e713ec84a0963
andrejtarasov <tarasov_av@cs.vsu.ru>
2021-03-12 17:43:05 +0300

    login page

--- tpUser/Karkasson/src/app/app-routing.module.ts
+++ tpUser/Karkasson/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginPageComponent} from './login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginPageComponent},
+  // {path: 'home', component: LoginPageComponent, data: {
+  //     page: 'home'
+  //   }},
+  // {path: 'about', component: LoginPageComponent, data: {
+  //     page: 'about'
+  //   }},
+  // {path: 'contact', component: LoginPageComponent, data: {
+  //     page: 'contact'
+  //   }},
+  // {path: '**', redirectTo: '/home', pathMatch: 'full'}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- tpUser/Karkasson/src/app/app.module.ts
+++ tpUser/Karkasson/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule} from '@angular/forms';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    MainPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

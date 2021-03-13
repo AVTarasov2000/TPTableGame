b16e7322bb491eb6e3191db6da664e91e1bc9615
andrejtarasov <tarasov_av@cs.vsu.ru>
2021-03-13 18:03:53 +0300

    toolBar navBar

--- tpUser/Karkasson/src/app/app.module.ts
+++ tpUser/Karkasson/src/app/app.module.ts
@@ -5,20 +5,36 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MainPageComponent } from './main-page/main-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { ToolBarComponent } from './tool-bar/tool-bar.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
     MainPageComponent,
+    NavigationBarComponent,
+    ToolBarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
     FormsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

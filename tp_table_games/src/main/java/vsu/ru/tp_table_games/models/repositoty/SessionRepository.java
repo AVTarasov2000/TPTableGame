684d4bbefbb40d1cbd9fab5c8308d3fc1c15f2b2
AntonovAleksandr <zzzz1917@bk.ru>
2021-05-09 03:11:29 +0300

    rep`s update

--- tp_table_games/src/main/java/vsu/ru/tp_table_games/models/repositoty/SessionRepository.java
+++ tp_table_games/src/main/java/vsu/ru/tp_table_games/models/repositoty/SessionRepository.java
@@ -1,10 +1,15 @@
 package vsu.ru.tp_table_games.models.repositoty;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import vsu.ru.tp_table_games.models.dtos.GameDto;
 import vsu.ru.tp_table_games.models.entities.Session;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SessionRepository extends PagingAndSortingRepository<Session, Long> {
     Optional<Session> findById(Long id);
+    List<Session> findAllBySessionStatus_Started();
+    List<Session> findAllByGame(GameDto gameDto);
+
 }
--- tp_table_games/src/main/java/vsu/ru/tp_table_games/models/repositoty/UserRepository.java
+++ tp_table_games/src/main/java/vsu/ru/tp_table_games/models/repositoty/UserRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
     Optional<User> findById(Long id);
+    Optional<User> findByLogin(String login);
 }

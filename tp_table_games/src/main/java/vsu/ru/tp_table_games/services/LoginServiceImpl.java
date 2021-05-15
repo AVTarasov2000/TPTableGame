be76b8431aa616839838b9773c37e3c8c4ae4c71
andrejtarasov <tarasov_av@cs.vsu.ru>
2021-05-15 14:50:32 +0300

    changed directory

--- tp_table_games/src/main/java/vsu/ru/tp_table_games/services/LoginServiceImpl.java
+++ tp_table_games/src/main/java/vsu/ru/tp_table_games/services/LoginServiceImpl.java
@@ -5,11 +5,8 @@ import vsu.ru.tp_table_games.models.andreysDTOs.UserDto;
 
 @Service
 public class LoginServiceImpl implements LoginService {
-    private int tmpId = 0;
     @Override
     public UserDto provide(UserDto userDto) {
-        userDto.setLogin(String.valueOf(tmpId));
-        tmpId+=1;
         return userDto; //todo
     }
 }
